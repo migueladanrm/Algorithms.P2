c8f7650af11f60eb163370223e70a5e0983a28f6
Miguel Rivas <migueladanrm@outlook.com>
2018-11-13 02:14:06 -0600

    Versión candidato Se ha implementado y comentado el código del proyecto.

--- TecGames/Program.cs
+++ TecGames/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using TecGames.Models;
 using static System.Console;
 
@@ -10,23 +9,87 @@ namespace TecGames
 {
     class Program
     {
+        private static int[] sets = new int[] { 50, 100, 200, 500, 1000 };
+        private static int generations = 25;
 
+        // espacio de trabajo que contiene la información a operar.
         private static Workspace workspace;
 
         static void Main(string[] args)
         {
-            int n = 1000;
+            WriteLine("ANÁLISIS DE ALGORITMOS\nII Proyecto Programado:Algoritmos Genético - Ramificación y poda \nIntegrantes:\n\t- Jose Carlo Hidalgo Chacón\n\t- Miguel Rivas Méndez\n");
 
-            workspace = new Workspace(n);
+            Write("En breve se iniciará la ejecución de los casos de prueba...");
+            Thread.Sleep(2500);
+            WriteLine();
 
-            //PrintJobs();
+            foreach (int n in sets) {
+                Console.Clear();
+                Write($"Iniciado caso de prueba para N={n} ...");
+                Thread.Sleep(1000);
+                WriteLine();
 
-            workspace.GnEvolve(25);
+                workspace = new Workspace(n);
 
-            //PrintJobs();
+                AuxWriteTitle("Algoritmo genético");
+                WriteLine($"---- Población inicial -----");
+                PrintWorkspaceOverview(workspace, n);
+
+                workspace.GnEvolve(generations);
+
+                WriteLine($"---- Después de aplicar algoritmo genético -----");
+                PrintWorkspaceOverview(workspace, n);
+
+                WriteLine("\nMEDICIONES:");
+                WriteLine($"Asignaciones: {workspace.measurementsGnAssignments}");
+                WriteLine($"Comparaciones: {workspace.measurementsGnComparisons}");
+                WriteLine($"Lineas ejecutadas: {workspace.measurementsGnAssignments + workspace.measurementsGnComparisons}");
+                Write($"Tiempo de ejecución: {workspace.measurementsGnTime.Elapsed.TotalMilliseconds} ms\n");
+
+                WriteLine();
+
+                AuxWriteTitle("Algoritmo de ramificación y poda");
+                workspace.RunBranchBound();
+
+                WriteLine($"---- Después de aplicar ramificación y poda -----");
+                PrintWorkspaceOverview(workspace, n);
+
+                WriteLine("\nMEDICIONES:");
+                WriteLine($"Asignaciones: {workspace.measurementsBbAssignments}");
+                WriteLine($"Comparaciones: {workspace.measurementsBbComparisons}");
+                WriteLine($"Lineas ejecutadas: {workspace.measurementsBbAssignments + workspace.measurementsBbComparisons}");
+                Write($"Tiempo de ejecución: {workspace.measurementsBbTime.Elapsed.TotalMilliseconds} ms\n\nPresione ENTER para continuar...");
+
+                ReadKey();
+                WriteLine();
+            }
+
+            Write("\n\nEl programa ha finalizado...");
 
             ReadKey();
+            ReadKey();
         }
+
+        /// <summary>
+        /// Imprime una visión general del espacio de trabajo.
+        /// </summary>
+        /// <param name="ws">Espacio de trabajo.</param>
+        /// <param name="n">Tamaño del arreglo actual.</param>
+        static void PrintWorkspaceOverview(Workspace ws, int n)
+        {
+            var schedules = new Dictionary<WorkSchedule, int> {
+                { WorkSchedule.AllDay, ws.Jobs.Where(j => j.WorkSection.Schedule == WorkSchedule.AllDay).Count() },
+                { WorkSchedule.MidDay, ws.Jobs.Where(j => j.WorkSection.Schedule == WorkSchedule.MidDay).Count() },
+                { WorkSchedule.AllNight, ws.Jobs.Where(j => j.WorkSection.Schedule == WorkSchedule.AllNight).Count() },
+                { WorkSchedule.MidNight, ws.Jobs.Where(j => j.WorkSection.Schedule == WorkSchedule.MidNight).Count() }
+            };
+
+            WriteLine($"Se tienen {schedules.Values.Sum() - 1} trabajos con un precio total de {workspace.GetJobsTotalPrice()} distribuido en:");
+            foreach (var kv in schedules)
+                WriteLine($"\t- {kv.Key}: {kv.Value}");
+        }
+
+        #region Métodos auxiliares
 
         static void PrintDesigners()
         {
@@ -88,5 +151,6 @@ namespace TecGames
             WriteLine($"\n{divider}\n");
         }
 
+        #endregion
     }
 }
--- TecGames/Workspace.cs
+++ TecGames/Workspace.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using TecGames.Models;
 
@@ -17,8 +18,6 @@ namespace TecGames
 
         private Random rnd = new Random(DateTime.Now.Millisecond);
 
-        private static int jobsId = 1;
-
         // cantidad de diseñadores por trabajo.
         private static int designersPerJob = 4;
 
@@ -26,10 +25,13 @@ namespace TecGames
         private static int gnCurrentGeneration = 0;
 
         // Mediciones
-        public static int measurementsGnAssignments = 0;
-        public static int measurementsGnComparisons = 0;
-        public static int measurementsBbAssignments = 0;
-        public static int measurementsBbComparisons = 0;
+        public int measurementsGnAssignments = 0;
+        public int measurementsGnComparisons = 0;
+        public int measurementsBbAssignments = 0;
+        public int measurementsBbComparisons = 0;
+
+        public Stopwatch measurementsGnTime;
+        public Stopwatch measurementsBbTime;
 
         /// <summary>
         /// Inicializa un espacio de trabajo.
@@ -37,7 +39,7 @@ namespace TecGames
         /// <param name="n">Cantidad de objetos.</param>
         public Workspace(int n)
         {
-            if (n < 10)
+            if (n < 50)
                 throw new InvalidOperationException();
 
             workSections = Seedbed.GenerateWorkSections();
@@ -94,14 +96,23 @@ namespace TecGames
         #region Métodos auxiliares
 
         /// <summary>
+        /// Obtiene el precio total de todos los trabajos.
+        /// </summary>
+        /// <returns>Precio total.</returns>
+        public double GetJobsTotalPrice() => jobs.Select(j => j.Designers.Select(d => d.Price).Sum()).Sum();
+
+        /// <summary>
         /// Obtiene una sección de trabajo correspondiente a su horario.
         /// </summary>
         /// <param name="schedule">Horario de trabajo.</param>
         /// <returns>Sección de trabajo.</returns>
         private WorkSection GetWorkSectionBySchedule(WorkSchedule schedule)
         {
-            if (workSections != null && schedule != WorkSchedule.NotAvailable)
+            if (workSections != null && schedule != WorkSchedule.NotAvailable) {
+                measurementsGnComparisons++;
                 return workSections.Where(ws => ws.Schedule == schedule).First();
+            }
+            measurementsGnComparisons++;
 
             return null;
         }
@@ -117,19 +128,26 @@ namespace TecGames
                 throw new InvalidOperationException($"No se acepta el valor '{schedule}' en esta operación.");
 
             var matches = new List<Location>();
+            measurementsGnAssignments++;
 
             switch (schedule) {
                 case WorkSchedule.AllDay:
                 case WorkSchedule.MidDay:
                     matches.AddRange(locations.Where(loc => loc.DayShift == WorkSchedule.AllDay));
-                    if (schedule == WorkSchedule.MidDay)
+                    if (schedule == WorkSchedule.MidDay) {
+                        measurementsGnComparisons++;
                         matches.AddRange(locations.Where(loc => loc.DayShift == WorkSchedule.MidDay));
+                    }
+                    measurementsGnComparisons++;
                     break;
                 case WorkSchedule.AllNight:
                 case WorkSchedule.MidNight:
                     matches.AddRange(locations.Where(loc => loc.NightShift == WorkSchedule.AllNight));
-                    if (schedule == WorkSchedule.MidNight)
+                    if (schedule == WorkSchedule.MidNight) {
+                        measurementsGnComparisons++;
                         matches.AddRange(locations.Where(loc => loc.NightShift == WorkSchedule.MidNight));
+                    }
+                    measurementsGnComparisons++;
                     break;
             }
 
@@ -144,6 +162,7 @@ namespace TecGames
         private Location GetRandomLocationByWorkSchedule(WorkSchedule schedule)
         {
             var matches = GetLocationsByWorkSchedule(schedule);
+            measurementsGnAssignments++;
             return matches[rnd.Next(0, matches.Count)];
         }
 
@@ -155,19 +174,27 @@ namespace TecGames
         public List<Designer> GetDesignersByWorkSchedule(WorkSchedule schedule)
         {
             var target = new List<Designer>();
+            measurementsGnAssignments++;
 
+            measurementsGnComparisons++;
             switch (schedule) {
                 case WorkSchedule.AllDay:
                 case WorkSchedule.MidDay:
                     target.AddRange(designers.Where(d => d.DayShift == WorkSchedule.AllDay));
-                    if (schedule == WorkSchedule.MidDay)
+                    if (schedule == WorkSchedule.MidDay) {
+                        measurementsGnComparisons++;
                         target.AddRange(designers.Where(d => d.DayShift == WorkSchedule.MidDay));
+                    }
+                    measurementsGnComparisons++;
                     break;
                 case WorkSchedule.AllNight:
                 case WorkSchedule.MidNight:
                     target.AddRange(designers.Where(d => d.NightShift == WorkSchedule.AllNight));
-                    if (schedule == WorkSchedule.MidDay)
+                    if (schedule == WorkSchedule.MidDay) {
+                        measurementsGnComparisons++;
                         target.AddRange(designers.Where(d => d.NightShift == WorkSchedule.MidNight));
+                    }
+                    measurementsGnComparisons++;
                     break;
             }
 
@@ -184,12 +211,21 @@ namespace TecGames
         {
             var randomDesigners = new List<Designer>();
             var designersBySchedule = GetDesignersByWorkSchedule(schedule);
+            measurementsGnAssignments += 2;
 
             while (randomDesigners.Count < n) {
+                measurementsGnComparisons++;
+
                 var tmpDesigner = designersBySchedule[rnd.Next(0, designersBySchedule.Count)];
-                if (!randomDesigners.Contains(tmpDesigner))
+                measurementsGnAssignments++;
+
+                if (!randomDesigners.Contains(tmpDesigner)) {
+                    measurementsGnComparisons++;
                     randomDesigners.Add(tmpDesigner);
+                }
+                measurementsGnComparisons++;
             }
+            measurementsGnComparisons++;
 
             return randomDesigners;
         }
@@ -204,7 +240,7 @@ namespace TecGames
 
         #region Algoritmo genético
 
-        Job[] gnParentJobs;
+        private Job[] gnParentJobs;
 
         /// <summary>
         /// Ejecuta el proceso de evolución.
@@ -212,31 +248,56 @@ namespace TecGames
         /// <param name="n">Cantidad de generaciones.</param>
         public void GnEvolve(int n)
         {
-            if (gnParentJobs == null)
+            measurementsGnTime = Stopwatch.StartNew();
+
+            if (gnParentJobs == null) {
+                measurementsGnComparisons++;
                 gnParentJobs = jobs.OrderBy(j => rnd.Next()).Take(2).ToList().ToArray();
+                measurementsGnAssignments++;
+            }
+            measurementsGnComparisons++;
 
             gnParentJobs = GnCrossover(gnParentJobs[0], gnParentJobs[1]);
-
             int fitness = 0;
 
+            measurementsGnAssignments += 2;
+
             for (int i = 0; i < n; i++) {
+                measurementsGnAssignments++;
+                measurementsGnComparisons++;
+
                 for (int j = 0; j < jobs.Count; j++) {
+                    measurementsGnAssignments += 2;
+                    measurementsGnComparisons++;
+
                     bool fit = GnEvaluate(jobs[j]);
                     if (fit) {
+                        measurementsGnComparisons++;
                         fitness++;
                     }
+                    measurementsGnComparisons++;
+                }
+                measurementsGnComparisons++;
+
+
+                if (((jobs.Count / 100) * fitness) / 100 < 50) {
+                    measurementsGnComparisons++;
+                    GnMutate();
+                } else {
+                    measurementsGnComparisons++;
+                    gnParentJobs = GnCrossover(gnParentJobs[0], gnParentJobs[1]);
+                    measurementsGnAssignments++;
                 }
 
-                if (((jobs.Count / 100) * fitness) / 100 < 50)
-                    GnMutate();
-                else
-                    gnParentJobs = GnCrossover(gnParentJobs[0], gnParentJobs[1]);
-
-                Console.WriteLine($"Generación {i}: {jobs.Select(j => j.Designers.Select(d => d.Price).Sum()).Sum()}");
+                //Console.WriteLine($"Generación {i}: {jobs.Select(j => j.Designers.Select(d => d.Price).Sum()).Sum()}");
 
                 fitness = 0;
                 gnCurrentGeneration++;
+                measurementsGnAssignments++;
             }
+            measurementsGnComparisons++;
+
+            measurementsGnTime.Stop();
         }
 
         /// <summary>
@@ -249,8 +310,11 @@ namespace TecGames
         {
             // Si tienen la misma sección de trabajo, se intercambia un diseñador y se agrega uno aleatorio.
             if (j1.WorkSection == j2.WorkSection) {
+                measurementsGnComparisons++;
+
                 var j1d = j1.Designers[rnd.Next(0, j1.Designers.Count)];
                 var j2d = j2.Designers[rnd.Next(0, j2.Designers.Count)];
+                measurementsGnAssignments += 2;
 
                 j1.Designers.RemoveAt(j1.Designers.IndexOf(j1d));
                 j2.Designers.RemoveAt(j2.Designers.IndexOf(j2d));
@@ -263,26 +327,37 @@ namespace TecGames
 
                 var designerCandidatesForJ1 = GetDesignersByWorkSchedule(j1.WorkSection.Schedule);
                 var designerCandidatesForJ2 = GetDesignersByWorkSchedule(j2.WorkSection.Schedule);
+                measurementsGnAssignments += 2;
 
                 j1.Designers.Add(designerCandidatesForJ1[rnd.Next(0, designerCandidatesForJ1.Count)]);
                 j2.Designers.Add(designerCandidatesForJ2[rnd.Next(0, designerCandidatesForJ2.Count)]);
             } else {
+                measurementsGnComparisons++;
+
                 // Si la sección de trabajo es distinta, se modifica el trabajo con mayor costo al tipo del de menor costo.
                 var j1p = j1.Designers.Select(d => d.Price).Sum();
                 var j2p = j2.Designers.Select(d => d.Price).Sum();
 
+                measurementsGnAssignments += 2;
+
                 if (j1p <= j2p) {
+                    measurementsGnComparisons++;
+
                     j2.WorkSection = j1.WorkSection;
                     j2.Location = GetRandomLocationByWorkSchedule(j1.WorkSection.Schedule);
                     j2.Designers = GetRandomDesignersByWorkSchedule(j1.WorkSection.Schedule, designersPerJob);
-
                     j1.Designers[rnd.Next(0, j1.Designers.Count)].Price = rnd.Next((int)Math.Min(j1p, j2p), (int)Math.Max(j1p, j2p)) + Math.Round(rnd.NextDouble());
+
+                    measurementsGnAssignments += 4;
                 } else {
+                    measurementsGnComparisons++;
+
                     j1.WorkSection = j2.WorkSection;
                     j1.Location = GetRandomLocationByWorkSchedule(j2.WorkSection.Schedule);
                     j1.Designers = GetRandomDesignersByWorkSchedule(j2.WorkSection.Schedule, designersPerJob);
-
                     j2.Designers[rnd.Next(0, j2.Designers.Count)].Price = rnd.Next((int)Math.Min(j1p, j2p), (int)Math.Max(j1p, j2p)) + Math.Round(rnd.NextDouble());
+
+                    measurementsGnAssignments += 4;
                 }
             }
 
@@ -299,9 +374,13 @@ namespace TecGames
             var jp = job.Designers.Select(d => d.Price).Sum();
             var jpp = gnParentJobs[rnd.Next(0, 2)].Designers.Select(d => d.Price).Sum();
             var diff = jp - jpp;
+            measurementsGnAssignments += 3;
 
-            if (diff < 0 || diff <= jpp * 0.1)
+            if (diff < 0 || diff <= jpp * 0.1) {
+                measurementsGnComparisons++;
                 return true;
+            }
+            measurementsGnComparisons++;
 
             return false;
         }
@@ -312,17 +391,30 @@ namespace TecGames
         private void GnMutate()
         {
             foreach (var j in jobs) {
+                measurementsGnAssignments++;
+
                 if (j != gnParentJobs[0] && j != gnParentJobs[1]) {
+                    measurementsGnComparisons++;
+
                     j.WorkSection = GetRandomWorkSection();
                     j.Location = GetRandomLocationByWorkSchedule(j.WorkSection.Schedule);
                     j.Designers = GetRandomDesignersByWorkSchedule(j.WorkSection.Schedule, designersPerJob);
+                    measurementsGnAssignments += 3;
 
                     foreach (var d in j.Designers) {
                         var pjd = gnParentJobs[rnd.Next(0, 2)].Designers[rnd.Next(0, designersPerJob)];
-                        if (d.Price > pjd.Price)
+
+                        measurementsGnAssignments += 2;
+
+                        if (d.Price > pjd.Price) {
+                            measurementsGnComparisons++;
                             d.Price = rnd.Next((int)pjd.Price, (int)d.Price) + Math.Round(rnd.NextDouble(), 2);
+                            measurementsGnAssignments++;
+                        }
+                        measurementsGnComparisons++;
                     }
                 }
+                measurementsGnComparisons++;
             }
         }
 
@@ -330,27 +422,67 @@ namespace TecGames
 
         #region Ramificación y poda
 
-        private void Bb()
+        /// <summary>
+        /// Ejecuta la optimización de los trabajos.
+        /// </summary>
+        public void RunBranchBound()
         {
-            var ws = GetRandomWorkSection();
-            var designersBySchedule = GetDesignersByWorkSchedule(ws.Schedule);
-            Designer rootDesigner = designersBySchedule[rnd.Next(0, designersBySchedule.Count)];
+            measurementsBbTime = Stopwatch.StartNew();
 
-            var job = new Job(jobsId, $"Trabajo {jobsId}", ws, GetRandomLocationByWorkSchedule(ws.Schedule),
-                new List<Designer>() { rootDesigner });
+            BranchBound(jobs[rnd.Next(0, jobs.Count)]);
+            jobs = bbTmpSolutions;
 
-            int index = 0;
+            measurementsBbTime.Stop();
+        }
 
-            for (int i = 0; i < designersBySchedule.Count; i++) {
-                if (designersBySchedule[i].Price <= rootDesigner.Price) {
-                    while (job.Designers[index] != null) {
-                        if (job.Designers[index].Price <= rootDesigner.Price) {
-                            index++;
-                            job.Designers[index] = designersBySchedule[i];
-                        }
-                    }
+        private List<Job> bbTmpSolutions = new List<Job>();
+
+        /// <summary>
+        /// Obtiene el precio de un trabajo específico.
+        /// </summary>
+        /// <param name="j">Trabajo.</param>
+        /// <returns>Precio del trabajo.</returns>
+        public double BbGetJobPrice(Job j)
+        {
+            return j.Designers.Select(d => d.Price).Sum();
+        }
+
+        /// <summary>
+        /// Ejecuta el algoritmo de ramificación y poda sobre los trabajos operados por el algoritmo genético.
+        /// </summary>
+        /// <param name="root">Raíz.</param>
+        private void BranchBound(Job root)
+        {
+            if (jobs.Count < 1) {
+                measurementsBbComparisons++;
+                return;
+            }
+            measurementsBbComparisons++;
+
+            if (bbTmpSolutions.Count == 0) {
+                measurementsBbComparisons++;
+                bbTmpSolutions.Add(root);
+            }
+            measurementsBbComparisons++;
+
+            Job leaf = null;
+
+            while (jobs.Count > 0) {
+                measurementsBbComparisons++;
+
+                var tmp = jobs[0];
+                jobs.RemoveAt(jobs.IndexOf(tmp));
+                leaf = tmp;
+                measurementsBbAssignments += 2;
+
+                if (BbGetJobPrice(leaf) < BbGetJobPrice(root)) {
+                    measurementsBbComparisons++;
+                    bbTmpSolutions.Add(leaf);
                 }
             }
+            measurementsBbComparisons++;
+
+            BranchBound(leaf);
         }
 
         #endregion
